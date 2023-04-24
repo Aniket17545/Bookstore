7b6bcdc289bc9b43e22c785da7af374f9edcaa57
Aniket Sahu <aniketsahu1712000@gmail.com>
2023-04-25 01:18:40 +0530

    [Aniket]-Model and API for user created

---
+++ bookstore_Backend/controller/user.controller.js
@@ -0,0 +1,27 @@
+const User = require("../model/user.model");
+
+// API To Get All Users
+exports.getAllUser = async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+}
+
+// API To Get UserByID
+exports.getUserById = async (req, res) => {
+    try{
+        const user = await User.findById({_id: req.params.id});
+        res.json(user);
+    }
+    catch(err){
+        return res.status(404).send('User Does Not Exist With Given User ID')
+    }
+}
+
+// API To Delete User
+exports.deleteUser = (req, res) => {
+    User.findOneAndRemove({
+        _id: req.params.id
+    }).then((user) => {
+        res.send(user);
+    });
+}
--- bookstore_Backend/server.js
+++ bookstore_Backend/server.js
@@ -1,18 +1,27 @@
 // loading module
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 // startup the app
-const app = express();
+
 const port = 5500; 
 app.use(express.json());
-app.use(cors());
+app.use(cors('*'));
 
 // database connection
-mongoose.connect('mongodb://localhost:27017/BOOKSTORE',{
+mongoose.connect('mongodb://127.0.0.1:27017/BOOKSTORE',{
     useNewUrlParser: true, useUnifiedTopology: true
 })
+const db = mongoose.connection;
+db.on('error', (error) => (console.error(error)))
+db.once('open', () => console.log('Connected to Database'))
+
+const UserRoute = require("./route/user.routes");
+
+// User Route
+app.use('/user', UserRoute)
 
 // start the server
 app.listen(port, () => { console.log('Server Started on Port : 5500')});
